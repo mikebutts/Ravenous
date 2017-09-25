f2a8970062f3a08f95d9ebe7e03d5658098e248b
mikebutts <mgbutts@gmail.com>
2017-09-25 07:03:13 -0400

    part 1 of ravenous

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
+    );
+  }
+}
+
+export default App;
